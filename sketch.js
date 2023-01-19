b7e4dcd78130d205dbb843608f3997f9bef9963f
Harry1345 <121846806+Harry1345@users.noreply.github.com>
2023-01-18 21:08:35 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	ball=Bodies.circle(260,350,20,ball_options);
+	World.add(world,ball);
+
+    groundObj = new ground(width/2,670,width,20)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  groundObj.display()
+  ellipse(ball.position.x,ball.position.y,20,20)
+  drawSprites();
+ 
+}
+
+
+
